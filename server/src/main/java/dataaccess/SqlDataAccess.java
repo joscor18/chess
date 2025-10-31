5f0fc991e798d071a5bd0567da2351fc6faf35e4
Joselyn Cortes <joac2@byu.edu>
2025-10-30 23:56:18 -0600

    imp(p4) starting sqldata implementation

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -3,12 +3,15 @@ package dataaccess;
 import datamodel.AuthData;
 import datamodel.GameData;
 import datamodel.UserData;
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 import java.util.List;
 
 public class SqlDataAccess implements DataAccess{
     public SqlDataAccess() throws DataAccessException{
-        DatabaseManager.createDatabase();
+        configureDatabase();
     }
 
     @Override
@@ -59,5 +62,56 @@ public class SqlDataAccess implements DataAccess{
     @Override
     public List<GameData> getGames() {
         return List.of();
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = (Connection) DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    //else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("",ex);
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException{
+        DatabaseManager.createDatabase();
+        try(Connection conn = (Connection) DatabaseManager.getConnection()) {
+            for (String statement : createStatements){
+                try (var preparedStatement = conn.prepareStatement(statement)){
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }catch (SQLException ex){
+            throw new DataAccessException("",ex);
+        }
     }
 }
